e5695a23bb610aa2d3137af3a4b87d990afbfb94
psilore <psiloc@gmail.com>
2021-10-23 21:28:25 +0200

    added aos

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
-import aos from './aos'
+//import aos from './aos'
+import AOS from 'aos';
 import '../css/aos.css';
 import '../css/bootstrap.min.css';
 import '../css/custom.css';
 import '../css/font-awesome.css';
 
-aos.init({});
+
+AOS.init();
+
+const docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+
+console.log('bonk')
